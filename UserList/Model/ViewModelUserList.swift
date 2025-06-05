36a5fb888983f7287fa085549108d660e00682a7
Daniel Ganem <daniel.ganem@icloud.com>
2025-06-05 14:21:26 +0200

    transformation to MVVM

---
+++ UserList/Model/ViewModelUserList.swift
@@ -0,0 +1,40 @@
+//
+//  ModelView.swift
+//  UserList
+//
+//  Created by daniel ganem on 03/06/2025.
+//
+
+import Foundation
+
+@MainActor class ViewModelUserList: ObservableObject {
+    @Published private(set) var users: [User] = []
+    @Published var isLoading = false
+    @Published var isGridView: Bool = false
+
+    private let repository = UserListRepository()
+    
+    func shouldLoadMoreData(currentItem item: User) -> Bool {
+        guard let lastItem = users.last else { return false }
+        return !isLoading && item.id == lastItem.id
+    }
+
+    // MARK: Loading
+    func fetchUsers() {
+        isLoading = true
+        Task {
+            do {
+                let users = try await repository.fetchUsers(quantity: 20)
+                self.users.append(contentsOf: users)
+                isLoading = false
+            } catch {
+                print("Error fetching users: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func reloadUsers() {
+        users.removeAll()
+        fetchUsers()
+    }
+}
---
+++ UserList/Views/UserListView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct UserListView: View {
+    @ObservedObject private var viewModel: ViewModelUserList = ViewModelUserList()
+    
+    var body: some View {
+        NavigationView {
+            if !viewModel.isGridView {
+                List(viewModel.users) { user in
+                    NavigationLink(destination: UserDetailView(user: user)) {
+                        RectangularUserView(user: user)
+                    }
+                }
+                .navigationTitle("Users")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Picker(selection: $viewModel.isGridView, label: Text("Display")) {
+                            Image(systemName: "rectangle.grid.1x2.fill")
+                                .tag(true)
+                                .accessibilityLabel(Text("Grid view"))
+                            Image(systemName: "list.bullet")
+                                .tag(false)
+                                .accessibilityLabel(Text("List view"))
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            viewModel.reloadUsers()
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                                .imageScale(.large)
+                        }
+                    }
+                }
+            } else {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
+                        ForEach(viewModel.users) { user in
+                            NavigationLink(destination: UserDetailView(user: user)) {
+                                RoundUserView(user: user)
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Users")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Picker(selection: $viewModel.isGridView, label: Text("Display")) {
+                            Image(systemName: "rectangle.grid.1x2.fill")
+                                .tag(true)
+                                .accessibilityLabel(Text("Grid view"))
+                            Image(systemName: "list.bullet")
+                                .tag(false)
+                                .accessibilityLabel(Text("List view"))
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            viewModel.reloadUsers()
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                                .imageScale(.large)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct UserListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserListView()
+    }
+}
