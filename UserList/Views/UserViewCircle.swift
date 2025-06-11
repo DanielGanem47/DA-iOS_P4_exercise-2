9f061f7649eed776e8edeee45d0022a4ebebc297
Daniel Ganem <daniel.ganem@icloud.com>
2025-06-11 17:10:52 +0200

    Ajout de preview

--- UserList/Views/UserViewCircle.swift
+++ UserList/Views/UserViewCircle.swift
@@ -30,3 +30,19 @@ struct UserViewCircle: View {
         }
     }
 }
+
+struct UsrViewCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        let userResponse = UserListResponse.User(
+            name: UserListResponse.User.Name(title: "Mr", first: "John", last: "Doe"),
+            dob: UserListResponse.User.Dob(date: "1990-01-01", age: 30),
+            picture: UserListResponse.User.Picture(
+                large: "https://randomuser.me/api/portraits/men/4.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/4.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/4.jpg"
+            )
+        )
+        let user = User(user: userResponse)
+        return UserViewCircle(user: user)
+    }
+}
